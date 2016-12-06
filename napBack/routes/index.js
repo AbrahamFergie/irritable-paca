2d8ee0bc9697a7fb4c079a1aadca6bddacc46832
Abraham Ferguson <abrahamferguson@Abrahams-MacBook-Pro.local>
2016-12-06 12:22:30 -0800

    got chat window to display through localhost

--- napBack/routes/index.js
+++ napBack/routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
+var router = require('express')()
+var http = require('http').Server(router)
+var io = require('socket.io')(http)
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html')
+})
 
-module.exports = router;
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg)
+  })
+})
+
+http.listen(3000, function(){
+  console.log('listening on *:3000')
+})
+module.exports = router
