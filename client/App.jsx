bc54cf33e4c530207c66dd2207b32f99abc2da60
thamaranth <thamaranth@gmail.com>
2016-12-09 19:11:04 -0800

    creating working version

--- client/App.jsx
+++ client/App.jsx
@@ -1,21 +1,20 @@
 import React, { Component } from 'react'
-import {ContainerView} from './go'
+import {ContainerView} from './go.jsx'
 import ReactDOM from 'react-dom'
-import {ChatApp} from './Chat'
+import {ChatApp} from './Chat.jsx'
 
 
 const Htmlroot = document.getElementById( 'app' )
-
-
 
  class App extends Component {
   constructor(props){
     super(props)
   }
   render() {
-    return ( <div><ContainerView/>
-                  <ChatApp/></div>
-
+    return ( <div>
+              <ChatApp />
+              <ContainerView />
+             </div>
   )
  }
 }
